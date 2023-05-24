460877da685dbd3733848b8f443406084872cee1
Efrim Dragos Alexandru <efrimdragos@gmail.com>
2023-05-25 00:35:56 +0300

    connect database

--- src/main/java/com/example/fire_department/FireDepartmentApplication.java
+++ src/main/java/com/example/fire_department/FireDepartmentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FireDepartmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FireDepartmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FireDepartmentApplication.class, args);
+    }
 
 }

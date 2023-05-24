1864fa762f9efcbf4d1b0f0de5b15f1a937ab590
Efrim Dragos Alexandru <efrimdragos@gmail.com>
2023-05-25 00:35:21 +0300

    Initial commit

---
+++ src/main/java/com/example/fire_department/FireDepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.fire_department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireDepartmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fire_department/FireDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fire_department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireDepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
